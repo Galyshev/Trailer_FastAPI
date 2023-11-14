2625b24bb42c73602de3362c748334bef82fa217
Roman <galyshev.roman@gmail.com>
2023-11-14 11:17:08 +0200

    0.6 Изменение концепции. Изменение структуры.

--- src/trailers/site_graber.py
+++ src/trailers/site_graber.py
@@ -1,13 +1,16 @@
+import re
 from datetime import date
+
 import requests
+from bs4 import BeautifulSoup
 from fastapi import Depends
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
-from sqlalchemy import select, insert
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.alchemy import get_async_session
-from src.trailers.model_bd import trailers, film_info
+from src.trailers.model_bd import trailers
 
 
 async def BS(link):
@@ -45,6 +48,7 @@ async def link_graber(session: AsyncSession = Depends(get_async_session)):
         part_link = str(part["href"])
         all_list.append(part_link)
 
+
     driver.close()
 
     for i in range(1, len(all_list) - 1, 2):  # шаг по нечетным, что бы не вносились ссылки дважды
@@ -57,81 +61,34 @@ async def link_graber(session: AsyncSession = Depends(get_async_session)):
 
         if len(chk) == 0:
             # добавление в базу
-            # link_trailer: str = 'https://www.imdb.com' + all_list[i]
-            link_trailer: str = 'https://www.imdb.com/title/' + id_film
+            link_film: str = 'https://www.imdb.com/title/' + id_film
+            link_trailer: str = 'https://www.imdb.com/video/' + id_video
             status: str = 'no_view'
             date_update: str = str(date.today())
+            # поиск ссылки на обложку на сайте imdb (она не прямая)
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            r = requests.get(link_film, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            # название фильма
+            title = soup.find('h1').text
+            # ссылка на обложку
+            cover_link = 'https://www.imdb.com' + soup.find("a", {'href': re.compile('..\/mediaviewer/.*')})['href']
+            # поиск прямой ссылки с сайта Амазона
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            r = requests.get(cover_link, headers=headers)
+            soup_cover = BeautifulSoup(r.text, 'html.parser')
+            cover_link_part = cover_link.split('/')[-2] + '-curr'
+            cover = soup_cover.find("div", {'class': 'ghbUKT'}).find("img", {'data-image-id': cover_link_part})['src']
+            # сохранение обложки на диск, что бы не парсить постоянно и сэкономить времяю Потом картинки будут удаляться
+            destination = "./cover/" + id_film + ".jpg"
+            r = requests.get(cover, stream=True)
+            with open(destination, 'wb') as f:
+                f.write(r.content)
+
             add_trailer = {"id_film": id_film, "id_video": id_video, "link_trailer": link_trailer,
-                           "status": status, "date_update": date_update}
-            stmt = insert(trailers).values(add_trailer)
-            await session.execute(stmt)
-            await session.commit()
-
-        # Прямая ссылка на видео трейлера
-            site = 'https://www.imdb.com/video/' + id_video
-            options = Options()
-            options.add_argument('--headless')
-            driver = webdriver.Firefox(options=options)
-            driver.get(site)
-            pageSource = driver.page_source
-            actual_trailer_soup = BeautifulSoup(pageSource, 'html.parser')
-            actual_trailer_tmp = actual_trailer_soup.find("div", {"class": "jw-media"}).find("video")
-            actual_trailer_link = actual_trailer_tmp["src"]
-            # print(actual_trailer_link)
-
-            driver.close()
-
-            # Прямая ссылка на обложку
-            options = Options()
-            options.add_argument('--headless')
-            driver = webdriver.Firefox(options=options)
-            driver.get(link_trailer)
-            pageSource = driver.page_source
-            content_soup = BeautifulSoup(pageSource, 'html.parser')
-
-            cover_tmp = "https://www.imdb.com" + content_soup.find("div", {"class": "ZYxwn"}).find('a')["href"]
-            cover_soup_image = await BS(cover_tmp)
-            cover_image_tmp = cover_soup_image.findAll("div", {"class": "kEDMKk"})
-            for i in cover_image_tmp:
-                peek_find = str(i).find("peek")
-                if peek_find == -1:
-                    cover_image = i.find("img")["src"]
-            # print(cover_image)
-
-            # Название
-            try:
-                title: str = content_soup.find("div", {"class": "EpHJp"}).text.replace('Original title:', '').strip()
-            except AttributeError as e:
-                title: str = content_soup.find("span", {"class": "fDTGTb"}).text
-            # print(title)
-            # Жанры
-            genres = content_soup.findAll("span", {"class": "ipc-chip__text"})
-            genre_list: list = []
-            for gnr in genres:
-                genre_list.append(gnr.text)
-            genre: str = ''
-            for gnr in genre_list:
-                genre = genre + gnr + ' | '
-            genre = genre[:-17].strip()
-            # print(genre)
-            try:
-                releases = content_soup.find("div", {"class": "kPHQBc"}).text
-            except AttributeError as e:
-                print(e)
-                releases: str = 'no date'
-            # print(releases)
-
-            try:
-                annotation = content_soup.find("span", {"class": "eVLpWt"}).text
-            except AttributeError as e:
-                annotation: str = 'no annotation'
-            # print(annotation)
-
-            driver.close()
-
-            add_info_films = {"id_film": id_film, "actual_trailer_link": actual_trailer_link, "cover_image": cover_image,
-                           "title": title, "genre": genre, "releases": releases,"annotation": annotation}
-            stmt = insert(film_info).values(add_info_films)
-            await session.execute(stmt)
-            await session.commit()
-
+                           'link_film': link_film,
+                           "status": status, "date_update": date_update, "title": title}
+            # stmt = insert(trailers).values(add_trailer)
+            # await session.execute(stmt)
+            # await session.commit()
+            print(add_trailer)
